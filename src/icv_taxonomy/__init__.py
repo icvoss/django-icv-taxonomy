a45c08f76fc3a749e8705038721517bd4f26e8ce
Nigel Copley <nigel@admoo.com>
2026-08-09 15:08:12 +0100

    chore(release): 1.0.0 (#9)
    Co-authored-by: Nigel Copley <npcopley@gmail.com>

--- src/icv_taxonomy/__init__.py
+++ src/icv_taxonomy/__init__.py
@@ -1,5 +1,5 @@
 """icv-taxonomy — Cross-domain taxonomy, vocabularies, and tagging for Django."""
 
-__version__ = "0.6.0"
+__version__ = "1.0.0"
 
 default_app_config = "icv_taxonomy.apps.IcvTaxonomyConfig"
